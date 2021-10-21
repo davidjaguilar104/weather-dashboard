f509c9b162786e6f25767d36ef3a2caa35902ad9
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-10-21 11:30:25 -0500

    added functions to get user input for the city

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,44 @@
 
 
 
+var userFormEl = document.getElementById("user-form");
+var cityInputEl = document.getElementById("city");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var citySearched = cityInputEl.value.trim();
+
+    if(citySearched) {
+        getLatLong(citySearched);
+        cityInputEl.textContent = "";
+    }
+    else {
+        alert("Please enter a city!");
+    }
+}
+
+
+
+var getLatLong = function(city) {
+    console.log(city)
+
+    var geocodingApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDYEKHzP-co-gS0nRRcHFgcuLug8_L8WQ4"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -9,5 +47,12 @@
 
 var getCityWeather = function(city) {
     
+    // might pass in lat and lon bases on variables from getCitySearched function, also remember exclue exists
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=f41131e2a68abf0f2b5d80a0cda7823f"
 }
+
+
+
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
---
+++ pseudocode.md
@@ -0,0 +1,1 @@
+1. Use google geocoding api to get lat and long of a city searched to later pass into function api url for OpenWeather api
