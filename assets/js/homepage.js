84d47edfb408704c96fa95796e25821c2b4b26e2
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-10-21 10:47:47 -0500

    added my api key to api url

---
+++ assets/js/homepage.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+var getCityWeather = function(city) {
+    
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=f41131e2a68abf0f2b5d80a0cda7823f"
+}
